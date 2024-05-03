e3ac661dcb40d2efa970380c20844b3efad92ad6
dd-nd <lya.yop@yandex.ru>
2024-05-03 10:20:57 +0300

    update

--- db/create.py
+++ db/create.py
@@ -1,6 +1,6 @@
-import sqlite3
+import sqlite3 as sq
 
-with sqlite3.connect('db/database.db') as con:
+with sq.connect('db/database.db') as con:
     cur = con.cursor()
     cur.execute('''CREATE TABLE IF NOT EXISTS users(
                 id INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -11,17 +11,23 @@ with sqlite3.connect('db/database.db') as con:
     cur.execute('''CREATE TABLE IF NOT EXISTS types(
                 id INTEGER PRIMARY KEY AUTOINCREMENT, 
                 name varchar)''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS statuses(
+                id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                name varchar)''')
     cur.execute('''CREATE TABLE IF NOT EXISTS contents(
                 id INTEGER PRIMARY KEY AUTOINCREMENT, 
                 name varchar, 
                 genre_id INTEGER, 
                 type_id INTEGER, 
+                status_id INTEGER,
                 user_id INTEGER, 
                 FOREIGN KEY (genre_id) references genres(id),
                 FOREIGN KEY (type_id) references types(id),
+                FOREIGN KEY (status_id) references statuses(id),
                 FOREIGN KEY (user_id) references users(id))''')
 
     cur.execute('''INSERT INTO genres (name) VALUES ('Боевик'), ('Комедия'), ('Драма'), 
                                                     ('Мелодрама'), ('Триллер'), ('Научная фантастика'), 
                                                     ('Фэнтези'), ('Ужасы'), ('Приключения'), ('Другое')''')
     cur.execute('''INSERT INTO types (name) VALUES ('Фильм'), ('Мультфильм'), ('Аниме')''')
+    cur.execute('''INSERT INTO statuses (name) VALUES ('Планирую'), ('В процессе'), ('Смотрел')''')
